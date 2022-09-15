9a882c2c48534e9860abecff0667da1c8f44664b
Yelyzaveta Omelich <omelichelizaveta@gmail.com>
2022-09-15 14:55:56 +0300

    hide-show password functionality

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import s from './Input.module.scss';
+import hide from '../../assets/hide.png';
+import show from '../../assets/show.png';
 
 interface IInputProps {
   placeholder: string;
@@ -6,10 +9,23 @@ interface IInputProps {
   label: string;
 }
 const Input = ({ placeholder, type, label }: IInputProps) => {
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
   return (
     <label className={s.label}>
       {label.toUpperCase()}
-      <input type={type} placeholder={placeholder} className={s.input} />
+      <input
+        type={passwordShown ? type : 'text'}
+        placeholder={placeholder}
+        className={s.input}
+      />
+      <button className={s.toggle} onClick={togglePassword}>
+        <img src={passwordShown ? show : hide} alt="hide" />
+      </button>
     </label>
   );
 };
